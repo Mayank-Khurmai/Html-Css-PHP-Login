826de60c623991cfaecf4f585c8442c6d2c33f6d
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-10 17:56:48 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -32,7 +32,37 @@ function col(){
     </body>
 </html>
         var b=rand(256);
-        var r3=rand(256);
+        var r3=rand(256);<!DOCTYPE html>
+<html>
+    <head>
+        <title>Angry Birds</title>
+        <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="../Images/favicon.ico">
+        <link rel = "stylesheet" href="../CSS/Style.css">
+    </head>
+    <body class="instruction">
+        <h1 id="heading">Click to Continue</h1>
+        <div align="center" class="ins">
+            <h1 style="margin-top: 20px">Instructions</h1>
+            <div class="list">
+                <li>The objective is to make all obstacles disappear in the given time limit in order to advanced to new level.</li>
+                <li>The obstacles will disappear when the "bird"comes in proximity of it.</li>
+                <li>The number of birds is limited.The player has to remove all obstacles and advance using the given number of words.</li>
+                <li>If the time runs out the player runs out of birds,the game ends.</li>
+                <li>When the game ends, the player has option to restart the game.</li> 
+            </div>
+        </div>
+        <script>
+            window.onmousedown = function(){
+                location.assign("../Levels/Level 1.html");
+            }
+        </script>
+    </body>
+</html>gktkg
+gflmg[lb
+bg]lbgltb
+b;gttblwmtm
+bmslmbslgt
         var g3=rand(256);
         var b3=rand(256);
         var id="l"+i;
